ebc18ac5b375544ca8dd966750b3030740ead4ae
ops <sandyvingataramin0@gmail.com>
2023-07-08 20:41:49 -0400

    test deploy

--- src/app/_document.tsx
+++ src/app/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { DocumentContext } from 'next/document'
+import Document, { DocumentContext,DocumentInitialProps } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 class MyDocument extends Document {
